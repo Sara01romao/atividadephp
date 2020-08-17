a505255bff2aa5f4886e221c7a645155b3527bd8
sara01romao <sara-romao@live.com>
2020-08-16 22:50:12 -0300

    .

--- index.php
+++ index.php
@@ -2,5 +2,19 @@
 //PHP Include & PHP Include_once 
 
 include 'header.php';  
+
+
+// array
+
+
+
+// $tipo_2= array(n=> 'arrys',...);
+
+$test2= array( "A", "B", "C");
+echo "<br>";
+echo $test2[1];
+
+
+
 ?>
 
